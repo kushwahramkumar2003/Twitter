8cbbad1cc48d484d02d9e1735cedebe4f14d0779
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-02-18 11:07:17 +0530

    Update styles and add new icons

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
+import { Inter, Quicksand } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+const quicksand = Quicksand({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={inter.className}>
+    <div className={quicksand.className}>
       <Component {...pageProps} />
     </div>
   );
