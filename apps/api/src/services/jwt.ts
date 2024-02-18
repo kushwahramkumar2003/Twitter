b263ada381378f55ace22f5a9c7b7844b1901897
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2024-02-19 00:14:26 +0530

    dotenv added

--- apps/api/src/services/jwt.ts
+++ apps/api/src/services/jwt.ts
@@ -1,8 +1,11 @@
 import JWT from "jsonwebtoken";
+import dotenv from "dotenv";
 import { prismaClient } from "../clients";
 import { User } from "@apollo/server/src/plugin/schemaReporting/generated/operations";
 
-const JWT_SECRET = "ai8wehsdbvjhabsdjfhbaw";
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET ?? "";
 
 class JWTService {
   public static generateTokenForUser(user: User) {
